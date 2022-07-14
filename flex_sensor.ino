495070d01755d7395e3ec717b224a7446511e05e
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-07-14 10:06:36 -0400

    Update flex_sensor.ino
    Recalibrate flex sensor

--- flex_sensor.ino
+++ flex_sensor.ino
@@ -2,6 +2,6 @@ const int flexSensorPin = A3;
 
 int getFlexSensor1() {
     int flexSensorReading = analogRead(flexSensorPin); 
-    int flexDegrees = map(flexSensorReading, 130, 265, 90, 180);
+    int flexDegrees = map(flexSensorReading, 670, 787, 90, 180);
     return flexDegrees;
 }
