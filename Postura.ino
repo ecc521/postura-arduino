11076bc5cd683ffb0bc6ec2addfbf00cc213e66d
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-07-18 09:39:40 -0400

    Update Postura.ino
    Rewrite to use structs

--- Postura.ino
+++ Postura.ino
@@ -1,42 +1,63 @@
 #include <ArduinoBLE.h>
+#include <vector>
 
 const int vibrationMotor = 3;
 
-
-//Postura Bluetooth Service. 
-BLEService posturaService("0cfbadd9-2593-4b4e-9bb6-1a459c706000");
-
-//Accelerometer and Gyroscope Characteristics. 
-//12 bytes: x, y, z, little endian (TODO: CURRENTLY USES DEVICE ENDIANNESS - should convert in case program run on big endian devices)
-union {
-  float axes[3];
-  byte bytes[12];
-} accReading1;
-
-union {
-  float axes[3];
-  byte bytes[12];
-} accReading2;
-
-union {
-  float axes[3];
-  byte bytes[12];
-} gyroReading1;
+//DataItem - Sensor sample groups. 
+struct DataItem {
+  //Accelerometer and gyroscope characteristics are 12 bytes: x, y, z, little endian 
+  //TODO: PROGRAM CURRENTLY USES DEVICE ENDIANNESS - should convert in case program run on big endian devices
+  union {
+    float axes[3];
+    byte bytes[12];
+  } acc1;
   
+  union {
+    float axes[3];
+    byte bytes[12];
+  } gyro1;
+  
+  union {
+    float axes[3];
+    byte bytes[12];
+  } acc2;
+
+  //Flex sensor characteristics. Degrees bend, integer. 
+  byte flex1;
+};
+
+std::vector<DataItem> dataBuffer;
+
+DataItem getLatestReading() {
+  DataItem reading;
+  
+  getAccelerometer(reading.acc1.axes);
+  getGyroscope(reading.gyro1.axes);
+  getExternalAccelerometer(reading.acc2.axes);
+  reading.flex1 = getFlexSensor1();
+}
+
+
+BLEService posturaService("0cfbadd9-2593-4b4e-9bb6-1a459c706000");
 BLECharacteristic accelerometer1("0cfbadd9-2593-4b4e-9bb6-1a459c706010", BLERead | BLENotify, 12, true);
 BLECharacteristic accelerometer2("0cfbadd9-2593-4b4e-9bb6-1a459c706011", BLERead | BLENotify, 12, true);
 BLECharacteristic gyroscope1("0cfbadd9-2593-4b4e-9bb6-1a459c706020", BLERead | BLENotify, 12, true);
-
-//Flex sensor characteristics. Degrees bend, integer. 
 BLEByteCharacteristic flexSensor1("0cfbadd9-2593-4b4e-9bb6-1a459c706030", BLERead | BLENotify);
 
 //Vibration motor. Off is 0, anything else is on. 
 BLEByteCharacteristic vibration("0cfbadd9-2593-4b4e-9bb6-1a459c706040", BLERead | BLEWrite);
 
+
+void updateCharacteristics(DataItem reading) {
+  accelerometer1.writeValue(reading.acc1.bytes, 12);
+  gyroscope1.writeValue(reading.gyro1.bytes, 12);
+  accelerometer2.writeValue(reading.acc2.bytes, 12);
+  flexSensor1.writeValue(reading.flex1);
+}
+
+
 void setupBLE() {
-    if (!BLE.begin()) {
-    while (1);
-  }
+  if (!BLE.begin()) {while (1);}
 
   BLE.setLocalName("Postura");
   BLE.setAdvertisedService(posturaService);
@@ -44,13 +65,11 @@ void setupBLE() {
   posturaService.addCharacteristic(accelerometer1);
   posturaService.addCharacteristic(accelerometer2);
   posturaService.addCharacteristic(gyroscope1);
-
   posturaService.addCharacteristic(flexSensor1);
 
   posturaService.addCharacteristic(vibration);
 
   BLE.addService(posturaService);
-
   BLE.advertise();
 }
 
@@ -65,7 +84,6 @@ void setup() {
 }
 
 
-
 void loop() {
   BLE.poll();
 
@@ -78,13 +96,6 @@ void loop() {
     digitalWrite(vibrationMotor, HIGH);
   }
 
-  getAccelerometer(accReading1.axes);
-  getGyroscope(gyroReading1.axes);
-  getExternalAccelerometer(accReading2.axes);
-
-  accelerometer1.writeValue(accReading1.bytes, 12);
-  gyroscope1.writeValue(gyroReading1.bytes, 12);
-  accelerometer2.writeValue(accReading2.bytes, 12);
-
-  flexSensor1.writeValue(getFlexSensor1());
+  DataItem latestReading = getLatestReading();
+  updateCharacteristics(latestReading);
 }
